01d5c03bcda5dd6356b300923536277b077064b7
William Hurley <wjhurley@comcast.net>
2019-09-12 23:22:00 -0700

    Update Player entity to match schema design and add SkillLevel entity

--- server/src/entity/Player.ts
+++ server/src/entity/Player.ts
@@ -1,30 +1,59 @@
 import {
   Column,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryColumn,
 } from 'typeorm';
 
+import { SkillLevel } from './SkillLevel';
+
 @Entity()
 export class Player {
-  @PrimaryColumn('int')
-  id: number;
+  @PrimaryColumn({
+    name: 'player_id',
+    nullable: false,
+    type: 'int',
+  })
+  playerId: number;
 
   @Column({
     length: 25,
+    name: 'first_name',
+    nullable: false,
   })
   firstName: string;
 
   @Column({
-    length: 25,
+    length: 50,
+    name: 'last_name',
+    nullable: false,
   })
   lastName: string;
+
+  @Column({
+    length: 50,
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    length: 50,
+    nullable: false,
+  })
+  pass: string;
+
+  @Column({
+    default: 'CURRENT_TIMESTAMP',
+    name: 'created_at',
+    nullable: false,
+    type: 'timestamptz',
+  })
+  createdAt: Date;
   
-  @Column('smallint')
-  skillLevel: number;
-
-  @Column('smallint')
-  teamCaptain: number;
-
-  @Column('int')
-  teamId: number;
+  @ManyToOne(type => SkillLevel, skillLevel => skillLevel.players)
+  @JoinColumn({
+    name: 'skill_level',
+  })
+  skillLevel: SkillLevel;
 }
